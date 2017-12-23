968b899fd70b1b2c230f8d1259c1fb3443609043
Brian Schweer <brianschweer11@gmail.com>
2017-12-23 16:50:33 -0600

    Initial Commit

---
+++ Bongo Plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bongo Plus
+//
+//  Created by Brian Schweer on 12/23/17.
+//  Copyright © 2017 Brian Schweer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
