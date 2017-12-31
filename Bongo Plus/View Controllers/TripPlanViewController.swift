270de17013ff3cd2b6da9dc2539eba6d21d50a8c
Brian Schweer <brianschweer11@gmail.com>
2017-12-30 22:08:57 -0600

    Minor change

---
+++ Bongo Plus/View Controllers/TripPlanViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TripPlanViewController.swift
+//  Bongo Plus
+//
+//  Created by Brian Schweer on 12/30/17.
+//  Copyright © 2017 Brian Schweer. All rights reserved.
+//
+
+import Foundation
